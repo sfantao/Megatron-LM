1a08f6d719edc7e11443d9bddd93c64f7d68d018
Ville Komulainen <vmjkom@utu.fi>
2025-03-18 15:38:34 +0200

    Clean up slurm script and provide comments for environment variables + Remove torchrun from launch script

--- slurm_scripts/launch.sh
+++ slurm_scripts/launch.sh
@@ -5,22 +5,18 @@
 export MIOPEN_USER_DB_PATH="/tmp/$(whoami)-miopen-cache-$SLURM_NODEID"
 export MIOPEN_CUSTOM_CACHE_DIR=$MIOPEN_USER_DB_PATH
 
-# Report affinity
-#echo "Rank $SLURM_PROCID --> $(taskset -p \$\$)"
 # Start conda environment inside the container
 $WITH_CONDA
+source /scratch/project_462000615/villekom/sam_container_venv/bin/activate
 # Set interfaces to be used by RCCL.
 # This is needed as otherwise RCCL tries to use a network interface it has
-# noa ccess to on LUMI.
-export NCCL_SOCKET_IFNAME=hsn0,hsn1,hsn2,hsn3
+# no access to on LUMI.
+#Use high speed NIC instead of mnt, or something else slower
+export NCCL_SOCKET_IFNAME=hsn
+#Use GPU Direct RDMA when GPU and NIC are on the same NUMA node
+#This might cause hangs and other performance issues
 export NCCL_NET_GDR_LEVEL=PHB
-#export NCCL_DMABUF_ENABLE=1
-export HSA_FORCE_FINE_GRAIN_PCIE=1
 
-
-unset CXI_FORK_SAFE
-unset CXI_FORK_SAFE_HP
-unset FI_CXI_DISABLE_CQ_HUGETLB
 export LD_LIBRARY_PATH=/pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/shs-libcxi-install/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/libfabric-install-master/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/aws-ofi-rccl2-install:$LD_LIBRARY_PATH
@@ -30,24 +26,15 @@ if [[ $SLURM_PROCID -eq 0 ]]; then
     echo "LINK LIBRARY" $LD_LIBRARY_PATH
 fi
 
+#Sanity check for cxi plugin and libfabric links
 if [ "$SLURM_PROCID" -eq 0 ]; then
-    echo "LDD librccl" $(ldd /pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-19/aws-ofi-rccl-install/librccl-net.so)
+    echo "LDD librccl" $(ldd /pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/aws-ofi-rccl2-install/librccl-net.so)
     echo "LD_LIBRARY" $LD_LIBRARY_PATH
-    echo "LDD libfabric.so" $(ldd /scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-19/libfabric-install-1.22.0/lib/libfabric.so)
-    #fi_info -p cxi
+    echo "LDD libfabric.so" $(ldd /scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/libfabric-install-master/lib/libfabric.so)
 fi
 
 export RANK=$SLURM_PROCID
 export LOCAL_RANK=$SLURM_LOCALID
 export WORLD_SIZE=$((8 * SLURM_NNODES))
-#echo "WORLD_SIZE IS:" $WORLD_SIZE
-#echo "LOCAL_ID:" $SLURM_LOCALID
 
 python3 -u "$@"
-#torchrun \
-#    --nproc-per-node=8 \
-#    --nnodes $SLURM_NNODES \
-#    --node_rank $SLURM_PROCID \
-#    --rdzv_backend c10d \
-#    --rdzv_endpoint $MASTER_ADDR:$MASTER_PORT \
-#    "$@"
