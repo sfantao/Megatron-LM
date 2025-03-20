90ec9296a4bec4c3e1a353f4703bc29d62a22496
Ville Komulainen <vmjkom@utu.fi>
2025-03-20 12:57:31 +0200

    Put unsets of fork variables back in

--- slurm_scripts/launch.sh
+++ slurm_scripts/launch.sh
@@ -10,12 +10,14 @@ $WITH_CONDA
 source /scratch/project_462000615/villekom/sam_container_venv/bin/activate
 # Set interfaces to be used by RCCL.
 # This is needed as otherwise RCCL tries to use a network interface it has
-# no access to on LUMI.
-#Use high speed NIC instead of mnt, or something else slower
-export NCCL_SOCKET_IFNAME=hsn
-#Use GPU Direct RDMA when GPU and NIC are on the same NUMA node
-#This might cause hangs and other performance issues
+# noa ccess to on LUMI.
+export NCCL_SOCKET_IFNAME=hsn0,hsn1,hsn2,hsn3
 export NCCL_NET_GDR_LEVEL=PHB
+
+
+unset CXI_FORK_SAFE
+unset CXI_FORK_SAFE_HP
+unset FI_CXI_DISABLE_CQ_HUGETLB
 
 export LD_LIBRARY_PATH=/pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/shs-libcxi-install/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/libfabric-install-master/lib:$LD_LIBRARY_PATH
