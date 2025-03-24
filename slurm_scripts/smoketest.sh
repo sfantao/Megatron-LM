6e465e2b90a333175391cf3ad37311334bb12b48
Ville Komulainen <vmjkom@utu.fi>
2025-03-24 16:10:24 +0200

    smoketest script for faulty NIC

---
+++ slurm_scripts/smoketest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export NCCL_SOCKET_IFNAME=hsn0,hsn1,hsn2,hsn3
+export NCCL_NET_GDR_LEVEL=3
+
+unset CXI_FORK_SAFE
+unset CXI_FORK_SAFE_HP
+unset FI_CXI_DISABLE_CQ_HUGETLB
+
+
+export LD_LIBRARY_PATH=/pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/shs-libcxi-install/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/libfabric-install-master/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/aws-ofi-rccl2-install:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/rccl-2025-02-26-85eb1f1/lib:$LD_LIBRARY_PATH
+
+/opt/rccltests/all_reduce_perf -o sum -b 4294967296 -e 4294967296 -i 41943040 -n 1
