bdc006f926a60bc1f3a5d93746c3610287668c57
Samuel Antao <samuel.antao@amd.com>
2025-04-11 12:41:38 +0300

    Use container with all components inside.

--- slurm_scripts/launch.sh
+++ slurm_scripts/launch.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -eu
 
 # MIOPEN needs some initialisation for the cache as the default location
 # does not work on LUMI as Lustre does not provide the necessary features.
@@ -6,34 +6,46 @@ export MIOPEN_USER_DB_PATH="/tmp/$(whoami)-miopen-cache-$SLURM_NODEID"
 export MIOPEN_CUSTOM_CACHE_DIR=$MIOPEN_USER_DB_PATH
 
 # Start conda environment inside the container
-$WITH_CONDA
+# $WITH_CONDA
 #Venv outside the container
-source venv/bin/activate
+# source venv/bin/activate
 # Set interfaces to be used by RCCL
 export NCCL_SOCKET_IFNAME=hsn0,hsn1,hsn2,hsn3
 export NCCL_NET_GDR_LEVEL=PHB
 
 
-unset CXI_FORK_SAFE
-unset CXI_FORK_SAFE_HP
-unset FI_CXI_DISABLE_CQ_HUGETLB
+# unset CXI_FORK_SAFE
+# unset CXI_FORK_SAFE_HP
+# unset FI_CXI_DISABLE_CQ_HUGETLB
 
-export LD_LIBRARY_PATH=/pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/shs-libcxi-install/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/libfabric-install-master/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/aws-ofi-rccl2-install:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/rccl-2025-02-26-85eb1f1/lib:$LD_LIBRARY_PATH
+# export LD_LIBRARY_PATH=/pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/shs-libcxi-install/lib:$LD_LIBRARY_PATH
+# export LD_LIBRARY_PATH=/pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/libfabric-install-master/lib:$LD_LIBRARY_PATH
+# export LD_LIBRARY_PATH=/pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/aws-ofi-rccl2-install:$LD_LIBRARY_PATH
+# export LD_LIBRARY_PATH=/pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/rccl-2025-02-26-85eb1f1/lib:$LD_LIBRARY_PATH
+
+#export LD_LIBRARY_PATH=/pfs/lustrep2/scratch/project_462000125/samantao/tickets/rccl-repros/from-ville/rccl-2025-02-26-85eb1f1-lib:$LD_LIBRARY_PATH
 
 if [[ $SLURM_PROCID -eq 0 ]]; then
     echo "LINK LIBRARY" $LD_LIBRARY_PATH
 fi
 
+
+export NCCL_DEBUG_FILE=/tmp/sfantao-nccl-logs-$SLURM_JOB_NAME-$SLURM_PROCID.log 
+
 #Sanity check for cxi plugin and libfabric links
 if [ "$SLURM_PROCID" -eq 0 ]; then
-    echo "LDD librccl" $(ldd /pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/aws-ofi-rccl2-install/librccl-net.so)
+    echo "LDD librccl" $(ldd /opt/aws-ofi-rccl/librccl-net.so)
     echo "LD_LIBRARY" $LD_LIBRARY_PATH
-    echo "LDD libfabric.so" $(ldd /scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/libfabric-install-master/lib/libfabric.so)
-    /scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/libfabric-install-master/bin/fi_info -p cxi
+    echo "LDD libfabric.so" $(ldd /opt/libfabric/lib/libfabric.so)
+    /opt/libfabric/bin/fi_info -p cxi
 fi
+
+# if [ "$SLURM_PROCID" -eq 0 ]; then
+#     echo "LDD librccl" $(ldd /pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/aws-ofi-rccl2-install/librccl-net.so)
+#     echo "LD_LIBRARY" $LD_LIBRARY_PATH
+#     echo "LDD libfabric.so" $(ldd /scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/libfabric-install-master/lib/libfabric.so)
+#     /scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/libfabric-install-master/bin/fi_info -p cxi
+# fi
 
 #Distributed args
 #These need to be set after srun launches, or use interpolation with bash -c and \
--- slurm_scripts/smoketest.sh
+++ slurm_scripts/smoketest.sh
@@ -3,14 +3,17 @@
 export NCCL_SOCKET_IFNAME=hsn0,hsn1,hsn2,hsn3
 export NCCL_NET_GDR_LEVEL=3
 
-unset CXI_FORK_SAFE
-unset CXI_FORK_SAFE_HP
-unset FI_CXI_DISABLE_CQ_HUGETLB
+# unset CXI_FORK_SAFE
+# unset CXI_FORK_SAFE_HP
+# unset FI_CXI_DISABLE_CQ_HUGETLB
 
 
-export LD_LIBRARY_PATH=/pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/shs-libcxi-install/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/libfabric-install-master/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/aws-ofi-rccl2-install:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/rccl-2025-02-26-85eb1f1/lib:$LD_LIBRARY_PATH
+# export LD_LIBRARY_PATH=/pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/shs-libcxi-install/lib:$LD_LIBRARY_PATH
+# export LD_LIBRARY_PATH=/pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/libfabric-install-master/lib:$LD_LIBRARY_PATH
+# export LD_LIBRARY_PATH=/pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/aws-ofi-rccl2-install:$LD_LIBRARY_PATH
+# export LD_LIBRARY_PATH=/pfs/lustrep3/scratch/project_462000394/containers/for-turkunlp-team/deps-2025-02-24/rccl-2025-02-26-85eb1f1/lib:$LD_LIBRARY_PATH
 
-/opt/rccltests/all_reduce_perf -o sum -b 4294967296 -e 4294967296 -i 41943040 -n 1
+#export LD_LIBRARY_PATH=/pfs/lustrep2/scratch/project_462000125/samantao/tickets/rccl-repros/from-ville/rccl-2025-02-26-85eb1f1-lib:$LD_LIBRARY_PATH
+
+# /opt/rccltests/all_reduce_perf -o sum -b 4294967296 -e 4294967296 -i 41943040 -n 1
+/opt/rccltests/all_reduce_perf -z 1 -b 2M -e 4096M -f 2 -g 1 -t 1 -R 1 -n 80 -w 5 -d half
